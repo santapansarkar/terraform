e7fbca92908db9dd1171ed09ec704056f28ed6b9
Santapan Sarkar <santapansarkar@gmail.com>
2021-11-16 00:00:06 +0530

    updated with more subnets

--- aws-network-self-modules/dev/main.tf
+++ aws-network-self-modules/dev/main.tf
@@ -5,10 +5,13 @@ provider "aws" {
 
 module "vpc" {
 
-  source                = "../modules/vpc"
-  cidr_range            = "10.0.0.0/16"
-  tenancy               = "default"
-  vpc_name              = "eb-vpc"
-  eks_subnet_cidr_range = "10.0.1.0/24"
+  source                          = "../modules/vpc"
+  cidr_range                      = "10.0.0.0/16"
+  tenancy                         = "default"
+  vpc_name                        = "eb-vpc"
+  eks_subnet_cidr_range           = "10.0.1.0/24"
+  dab_subnet_cidr_range           = "10.0.2.0/24"
+  control_vm_subnet_cidr_range    = "10.0.3.0/24"
+  forgerock_rms_subnet_cidr_range = "10.0.4.0/24"
 
 }
--- aws-network-self-modules/modules/vpc/variables.tf
+++ aws-network-self-modules/modules/vpc/variables.tf
@@ -10,6 +10,22 @@ variable "tenancy" {
 variable "vpc_name" {}
 
 variable "eks_subnet_cidr_range" {
-    default = "10.0.1.0/24"
+      default = "10.0.1.0/24"
   
 }
+
+variable "dab_subnet_cidr_range" {
+    default = "10.0.2.0/24"
+  
+}
+
+variable "control_vm_subnet_cidr_range" {
+    default = "10.0.3.0/24"
+  
+}
+
+variable "forgerock_rms_subnet_cidr_range" {
+    default = "10.0.4.0/24"
+  
+}
+
