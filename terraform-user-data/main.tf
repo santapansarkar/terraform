58a84bb40de4b6f8ca4567bb853254fe58114b22
Santapan Sarkar <santapansarkar@gmail.com>
2022-01-14 14:48:37 +0530

    cloud-init part added

---
+++ terraform-user-data/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  profile = "default"
+}
+
+
+data "template_file" "user_data"{
+    template = file("./user_data.yaml")
+}
+
+resource "aws_instance" "terraform_user_data" {
+    ami = var.ec2_ami_id
+    instance_type = var.ec2_instance_type
+    user_data = data.template_file.user_data.rendered
+    security_groups = ["ssh-access-default-vpc"]
+    tags = {
+      "Name" = "terraform-user-data"
+    }
+}
---
+++ terraform-user-data/variables.tf
@@ -0,0 +1,8 @@
+variable "ec2_instance_type" {
+    default = "t2.micro"
+  
+}
+
+variable "ec2_ami_id" {
+        default = "ami-08e4e35cccc6189f4"
+    }
