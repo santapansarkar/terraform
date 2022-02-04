0a3644be0debc776b26a72a671c59604291faa80
Santapan Sarkar <santapansarkar@gmail.com>
2022-02-04 08:27:26 +0530

    aws eks cluster module base definition

---
+++ terraform-aws-eks/modules/eks/cluster.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_cluster" "terraform_cluster"{
+
+    name = var.cluter_name
+    role = var.cluster_iam_role
+
+    vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
